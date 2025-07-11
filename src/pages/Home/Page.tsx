51ff757ceda4ae1125c1e9562ccf169e26427920
hikkywannafly <ggraygon@gmail.com>
2025-07-11 08:38:45 +0700

    up

--- src/pages/Home/Page.tsx
+++ src/pages/Home/Page.tsx
@@ -6,10 +6,7 @@ const Home: FC = () => {
       <section>
         <div className="flex  min-h-[calc(100vh-64px)] bg-base-200 w-full justify-center ">
           <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="https://media.tenor.com/wCtfQZz2VqoAAAAM/konosuba-chomusuke.gif"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
+
             <div>
               <h1 className="text-5xl font-bold">Welcome</h1>
             </div>
